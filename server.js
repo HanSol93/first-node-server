9b10d63f643279692323c25c461092dcb85a0f76
kakao_ent <kakao_ent@Elody-MacBookPro.local>
2020-06-09 15:39:02 +0900

    update

--- server.js
+++ server.js
@@ -1,35 +1,47 @@
 // 서버를 만드는 모듈을 불러옴 
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 // 서버 만드는 메서드 
 http.createServer((request, response) => {
-    return request
-        // 요청에 에러가 있으면 
-        .on('error', err => {
-            console.error(err);
-        })
-        // 요청에 데이터가 있으면
-        .on('data', data => {
-            console.log(data)
-        })
-        // 요청에 데이터가 모두 받아졌으면
-        .on('end', () => {
 
-            // 응답에 에러가 있으면
-            response.on('error', err => {
-                console.error(err);
-            })
+    // url에서 path 추출
+    const path = url.parse(request.url, true).pathname;
 
-            // 성공 상태 코드 
-            response.statusCode = 200;
+    // GET요청이면
+    if (request.method === 'GET') {
+        switch (path) {
+            case '/about':
+                // header 설정
+                response.writeHead(200, { 'Content-Type': 'text/html' })
+                    // 파일 읽는 메서드 
+                fs.readFile(__dirname + '/about.html', (err, data) => {
+                    if (err) {
+                        // 에러 발생시 에러 기록 후 종료 
+                        return console.error(err);
+                    }
+                    // 브라우저로 전송
+                    response.end(data, 'utf-8');
+                });
+                break;
 
-            // header 설정
-            response.setHeader('Content-Type', 'text/plain');
-
-            // body에 정보 탑재
-            response.write('hi\n');
-
-            // 정보 탑재 후 브라우저로 전송 
-            response.end('the end!');
-        })
+            case '/':
+                // header 설정
+                response.writeHead(200, { 'Content-Type': 'text/html' })
+                    // 파일 읽는 메서드 
+                fs.readFile(__dirname + '/index.html', (err, data) => {
+                    if (err) {
+                        // 에러 발생시 에러 기록 후 종료 
+                        return console.error(err);
+                    }
+                    // 브라우저로 전송
+                    response.end(data, 'utf-8');
+                });
+                break;
+            default:
+                response.statusCode = 404;
+                response.end('주소가 없습니다');
+        }
+    }
 }).listen(8080);
